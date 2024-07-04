5aab196718d77cc7991c0801aa5cd08227067c3d
Dinçer Tekin <dincertekin@protonmail.com>
2024-07-04 17:11:26 +0300

    First release

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace dtEyeCare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            notifyIcon = new NotifyIcon(components);
+            shortBreakTimer = new System.Windows.Forms.Timer(components);
+            longBreakTimer = new System.Windows.Forms.Timer(components);
+            twentyMinutesCheckBox = new CheckBox();
+            twoHoursCheckBox = new CheckBox();
+            remindMeLabel = new Label();
+            runAtStartupCheckBox = new CheckBox();
+            SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            notifyIcon.Icon = (Icon)resources.GetObject("notifyIcon.Icon");
+            notifyIcon.Text = "dtEyeCare";
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += notifyIcon_DoubleClick;
+            // 
+            // shortBreakTimer
+            // 
+            shortBreakTimer.Interval = 1200000;
+            shortBreakTimer.Tick += shortBreakTimer_Tick;
+            // 
+            // longBreakTimer
+            // 
+            longBreakTimer.Interval = 7200000;
+            longBreakTimer.Tick += longBreakTimer_Tick;
+            // 
+            // twentyMinutesCheckBox
+            // 
+            twentyMinutesCheckBox.AutoSize = true;
+            twentyMinutesCheckBox.Checked = true;
+            twentyMinutesCheckBox.CheckState = CheckState.Checked;
+            twentyMinutesCheckBox.Location = new Point(58, 95);
+            twentyMinutesCheckBox.Name = "twentyMinutesCheckBox";
+            twentyMinutesCheckBox.Size = new Size(116, 19);
+            twentyMinutesCheckBox.TabIndex = 0;
+            twentyMinutesCheckBox.Text = "20 minutes break";
+            twentyMinutesCheckBox.UseVisualStyleBackColor = true;
+            twentyMinutesCheckBox.CheckedChanged += twentyMinutesCheckBox_CheckedChanged;
+            // 
+            // twoHoursCheckBox
+            // 
+            twoHoursCheckBox.AutoSize = true;
+            twoHoursCheckBox.Checked = true;
+            twoHoursCheckBox.CheckState = CheckState.Checked;
+            twoHoursCheckBox.Location = new Point(58, 70);
+            twoHoursCheckBox.Name = "twoHoursCheckBox";
+            twoHoursCheckBox.Size = new Size(97, 19);
+            twoHoursCheckBox.TabIndex = 0;
+            twoHoursCheckBox.Text = "2 hours break";
+            twoHoursCheckBox.UseVisualStyleBackColor = true;
+            twoHoursCheckBox.CheckedChanged += twoHoursCheckBox_CheckedChanged;
+            // 
+            // remindMeLabel
+            // 
+            remindMeLabel.AutoSize = true;
+            remindMeLabel.Location = new Point(55, 52);
+            remindMeLabel.Name = "remindMeLabel";
+            remindMeLabel.Size = new Size(103, 15);
+            remindMeLabel.TabIndex = 1;
+            remindMeLabel.Text = "Remind me when:";
+            // 
+            // runAtStartupCheckBox
+            // 
+            runAtStartupCheckBox.AutoSize = true;
+            runAtStartupCheckBox.Checked = true;
+            runAtStartupCheckBox.CheckState = CheckState.Checked;
+            runAtStartupCheckBox.Location = new Point(55, 21);
+            runAtStartupCheckBox.Name = "runAtStartupCheckBox";
+            runAtStartupCheckBox.Size = new Size(100, 19);
+            runAtStartupCheckBox.TabIndex = 2;
+            runAtStartupCheckBox.Text = "Run at startup";
+            runAtStartupCheckBox.UseVisualStyleBackColor = true;
+            runAtStartupCheckBox.CheckedChanged += runAtStartupCheckBox_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(255, 138);
+            Controls.Add(runAtStartupCheckBox);
+            Controls.Add(remindMeLabel);
+            Controls.Add(twoHoursCheckBox);
+            Controls.Add(twentyMinutesCheckBox);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "dtEyeCare";
+            FormClosing += Form1_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon;
+        private System.Windows.Forms.Timer shortBreakTimer;
+        private System.Windows.Forms.Timer longBreakTimer;
+        private CheckBox twentyMinutesCheckBox;
+        private CheckBox twoHoursCheckBox;
+        private Label remindMeLabel;
+        private CheckBox runAtStartupCheckBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dtEyeCare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            addToStartup();
+
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            ToolStripMenuItem exitToolStripMenuItem = new ToolStripMenuItem("Exit");
+            exitToolStripMenuItem.Click += ExitToolStripMenuItem_Click;
+            contextMenuStrip.Items.Add(exitToolStripMenuItem);
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+        }
+
+        private void addToStartup()
+        {
+            string appName = "dtEyeCare";
+            string appPath = Application.ExecutablePath;
+
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rk.SetValue(appName, appPath);
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+            notifyIcon.ShowBalloonTip(30000, "dtEyeCare", "The application is still running in the system tray.", ToolTipIcon.Info);
+        }
+
+        private void shortBreakTimer_Tick(object sender, EventArgs e)
+        {
+            notifyIcon.BalloonTipTitle = "Short Break Time!";
+            notifyIcon.BalloonTipText = "20 minutes have passed! Take a 20-second break and look at something 20 feet away.";
+            notifyIcon.ShowBalloonTip(30000);
+
+            shortBreakTimer.Interval = 1200000;
+            shortBreakTimer.Start();
+        }
+
+        private void longBreakTimer_Tick(object sender, EventArgs e)
+        {
+            notifyIcon.BalloonTipTitle = "Long Break Time!";
+            notifyIcon.BalloonTipText = "2 hours have passed! Take a 15-minute break away from the screen.";
+            notifyIcon.ShowBalloonTip(30000);
+
+            longBreakTimer.Interval = 7200000;
+            longBreakTimer.Start();
+        }
+
+        private void twoHoursCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            longBreakTimer.Enabled = twoHoursCheckBox.Checked;
+        }
+
+        private void twentyMinutesCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            shortBreakTimer.Enabled = twentyMinutesCheckBox.Checked;
+        }
+
+        private void runAtStartupCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            string appName = "dtEyeCare";
+            string appPath = Application.ExecutablePath;
+
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            try
+            {
+                if (runAtStartupCheckBox.Checked)
+                {
+                    rk.SetValue(appName, appPath);
+                }
+                else
+                {
+                    if (rk.GetValue(appName) != null)
+                    {
+                        rk.DeleteValue(appName, false);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error accessing registry: {ex.Message}", "Registry Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
